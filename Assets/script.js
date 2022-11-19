9a67585348ddf10a2550b783ccf413d42c73665e
kristermyr <krister90@gmail.com>
2022-11-19 10:18:58 -0600

    exploring combinations of adding divs to classes in CSS to see their behavior on the page. connected row and time block which seems natural for the time slots

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,3 @@
-
-
 //  SUDOCODE 
 
 // 1. I want to create most assignments to start the code
@@ -9,3 +7,8 @@
 // 5. I want the user to be able to save the event to local storage
 // 6. I want the user to be able to refresh the page and the events will still be there
 // 7. I want the user to be able to clear the events from local storage
+
+
+var currentDayElement = $('#currentDay');
+var containerElement = $('.container');
+var currentHour = moment().hour();
