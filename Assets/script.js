2f5aeb39f8b97c4c91d1a7e384d906480327d504
kristermyr <krister90@gmail.com>
2022-11-20 13:41:24 -0600

    written text in input gets saved in local storage, does not stay in box when reloaded. Need to access local storage and set it back in on refresh

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,9 @@
 //  SUDOCODE 
 
 // 1. I want to create most assignments to start the code
-// 2. I want to add time blocks to the containers using jquery
+// X2. I want to add time blocks to the containers
 // 3. I want the calendar to be color coded to show past, present, and future
-// 4. I want to user to be able to click on the time block and enter an event
+// X4. I want to user to be able to click on the time block and enter an event
 // 5. I want the user to be able to save the event to local storage
 // 6. I want the user to be able to refresh the page and the events will still be there
 // 7. I want the user to be able to clear the events from local storage
@@ -21,4 +21,15 @@ function displayTime(){
   setInterval(displayTime, 
     
     1000);
+  
 
+$(document).ready(function() {
+  $('.saveBtn').on('click', function () {
+  
+  var textContent = $(this).siblings('.description').val();
+  var time = $(this).parent().attr('id');
+  
+  localStorage.setItem(time, textContent);
+
+})
+})
