37ffc763ca13defa876e900cdab4f093779ca5bf
kristermyr <krister90@gmail.com>
2022-11-25 11:22:19 -0600

    added change of classes based on time. Color in the calendar changes depending on current time

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,11 @@
 //  SUDOCODE 
 
-// 1. I want to create most assignments to start the code
+// X1. I want to create most assignments to start the code
 // X2. I want to add time blocks to the containers
 // 3. I want the calendar to be color coded to show past, present, and future
 // X4. I want to user to be able to click on the time block and enter an event
-// 5. I want the user to be able to save the event to local storage
-// 6. I want the user to be able to refresh the page and the events will still be there
-// 7. I want the user to be able to clear the events from local storage
-
+// X5. I want the user to be able to save the event to local storage
+// X6. I want the user to be able to refresh the page and the events will still be there
 
 //var currentDayElement = $('#currentDay');
 var containerElement = $('.container');
@@ -36,10 +34,33 @@ $(document).ready(function() {
 
 
 $('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
 
-function timeColorOverlay(){
+$(".time-block").each(function () {
+  var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
 
-}
+  if (blockTime < currentHour) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+  }
+  else if (blockTime === currentHour) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+  }
+  else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
 
-
+  }
+})
